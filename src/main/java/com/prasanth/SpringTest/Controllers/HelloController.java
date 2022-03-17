5c6de9cc4eb8a6fa10cd8a92058d061611d8f73c
Prasdey <prasanthdey@gmail.com>
2022-03-17 17:45:27 +0100

    fixed restcontroller

--- src/main/java/com/prasanth/SpringTest/Controllers/HelloController.java
+++ src/main/java/com/prasanth/SpringTest/Controllers/HelloController.java
@@ -1,14 +1,15 @@
 package com.prasanth.SpringTest.Controllers;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
-    @GetMapping("/")
+    @RequestMapping("/")
     public String index()  {
-        return "Greetings from Prasanth";
+        return "Greetings from Prasanth!";
 
     }
 }
